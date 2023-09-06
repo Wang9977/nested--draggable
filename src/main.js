e009fdabffa717a7b412bfa8eeb780fb86264722
Wang9977 <44893615+Wang9977@users.noreply.github.com>
2023-09-06 03:32:16 +0000

    test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
-// import router from './router'
 
 Vue.config.productionTip = false
 
@@ -11,10 +8,10 @@ Vue.config.productionTip = false
 import VueCompositionApi from '@vue/composition-api'
 Vue.use(VueCompositionApi)
 Vue.use(ElementUI)
-/* eslint-disable no-new */
 
 import CombineArea from '@/components/index'
 Vue.use(CombineArea)
+
 new Vue({
   render: (h) => h(App)
 }).$mount('#app')
