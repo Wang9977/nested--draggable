c41fe704ec1b2b64b5c494b19852f11b8c40db0e
Yona Wang <44893615+Wang9977@users.noreply.github.com>
2026-02-27 12:50:12 +0000

    feat: deploy

---
+++ dist/precache-manifest.cdd50a9018efa5a0702b0aed1778ea09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128633f5844dec11a583",
+    "url": "/nested--draggable/css/app.c2487dda.css"
+  },
+  {
+    "revision": "0181df8ebb3374abea0f",
+    "url": "/nested--draggable/css/chunk-vendors.d94a351e.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/nested--draggable/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/nested--draggable/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "da0865af40e89b8e8547f5c6145f03af",
+    "url": "/nested--draggable/index.html"
+  },
+  {
+    "revision": "128633f5844dec11a583",
+    "url": "/nested--draggable/js/app.407fa9cd.js"
+  },
+  {
+    "revision": "0181df8ebb3374abea0f",
+    "url": "/nested--draggable/js/chunk-vendors.206108c3.js"
+  },
+  {
+    "revision": "53a6c69ad21305ebc1a875c3840cc867",
+    "url": "/nested--draggable/manifest.json"
+  }
+]);
