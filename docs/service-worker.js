64ea6e8aed17e0b46856b61ee60cc4813ec02c2e
Yona Wang <44893615+Wang9977@users.noreply.github.com>
2026-04-01 22:18:33 +0800

    refactor: Vue 3 + Vite重构，样式优化

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nested--draggable/precache-manifest.10366b50973424fa17c68c3190af5ede.js"
+  "/nested--draggable/precache-manifest.cdd50a9018efa5a0702b0aed1778ea09.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "nested-dragable"});
+workbox.core.setCacheNameDetails({prefix: "nested-draggable"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
